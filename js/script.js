8a1a626f987d6da7fed08b297892f98ab33e6c96
KaterynaFomenko <4273007@gmail.com>
2022-07-07 18:50:31 +0200

    scss

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$('.close').click(function () {
+    $('.notification').slideUp(500);
+})
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:5
+            },
+
+            1000:{
+                items:5
+            }
+        }
+    });
+});
